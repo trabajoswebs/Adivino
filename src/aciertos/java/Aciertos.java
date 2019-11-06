4c8a51eace87c28e25c300c679310c9c8f2a34d3
Johan <>
2019-11-06 11:55:58 +0100

    Cambio de los valores aleatorios generados entre los números 10 y el 50

--- src/aciertos/java/Aciertos.java
+++ src/aciertos/java/Aciertos.java
@@ -6,8 +6,10 @@ public class Aciertos {
 
     public static void main(String[] args) {
         int n = 0,cont = 0;
-        int x = (int) (500 *Math.random());
-        //System.out.print(x);
+        int min = 10;
+        int max = 50;
+        int x = (int) (Math.random() * (max - min + 1)) + min;
+        //System.out.print(x);        
         Scanner sc = new Scanner(System.in);
         do {
             System.out.print("\nIntroduce el valor de X: ");
